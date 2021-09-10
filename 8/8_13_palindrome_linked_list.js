c590fd5fe942bde967c8166c35d386f4cd8c149a
FonDitbul <niki7084@naver.com>
2021-09-10 15:53:17 +0900

    8-13 palindrome list 문제

---
+++ 8/8_13_palindrome_linked_list.js
@@ -0,0 +1,42 @@
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+
+const example_input_1 = [1,2,2,1]
+const example_input_2 = [1,2]
+
+var isPalindrome = function(head) {
+    let left = null
+    let right = null
+    let temp;
+
+    let fast = head;
+    let slow = head;
+
+    while(fast && fast.next){
+        fast = fast.next.next
+        temp = slow.next;
+        slow.next = left;
+        left = slow;
+        slow = temp;
+    }
+    right = fast ? slow.next : slow;
+    while (left && right) {
+        if (left.val !== right.val) return false;
+        left = left.next;
+        right = right.next;
+    }
+    return true
+}
+
+console.log(isPalindrome(example_input_1))
+console.log(isPalindrome(example_input_2))
