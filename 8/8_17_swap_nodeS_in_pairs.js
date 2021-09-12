63a94a6a1278b99126e3e5e7c0a88e2510cf6f10
FonDitbul <niki7084@naver.com>
2021-09-12 21:29:56 +0900

    8-17 swap nodes in pairs runtime

--- 8/8_17_swap_nodeS_in_pairs.js
+++ 8/8_17_swap_nodeS_in_pairs.js
@@ -4,15 +4,25 @@ const example_input_1 = ""
 
 function solution(head){
     let cur = head;
-    let temp;
-    while (cur && cur.next){
-        temp = cur.next.val;
-        cur.next.val = cur.val;
-        cur.val = temp;
-
-        cur = cur.next.next;
+    while(cur){
+        let curVal = cur.val;
+        if(cur.next){
+            cur.val = cur.next.val;
+            cur.next.val = curVal;
+            cur = cur.next.next;
+        }else{
+            cur = cur.next;
+        }
     }
     return head
+    // while (cur && cur.next){
+    //     temp = cur.next.val;
+    //     cur.next.val = cur.val;
+    //     cur.val = temp;
+    //
+    //     cur = cur.next.next;
+    // }
+    // return head
 }
 
 console.log(solution(example_input_1))
